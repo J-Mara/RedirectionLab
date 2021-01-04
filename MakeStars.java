baac052f721dd51482adea30b4c789c9afebdb67
J-Mara <jmaragakis20@stuy.edu>
2021-01-04 13:18:04 -0500

    better base

--- MakeStars.java
+++ MakeStars.java
@@ -3,7 +3,12 @@ public class MakeStars{
   public static void main(String[] args) {
     Scanner n = new Scanner(System.in);
     while(n.hasNextLine()){
-      System.out.println(n.nextLine());
+      Scanner p = new Scanner(n.nextLine());
+      while(p.hasNext()){
+        System.out.print(p.next());
+        System.out.print(" ");
+      }
+      System.out.println("");
     }
   }
 }
