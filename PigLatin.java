f778f46d30a7e9bf03e31410bd53caec0820fa86
J-Mara <jmaragakis20@stuy.edu>
2021-01-05 21:54:22 -0500

    PigLatin with first method

--- PigLatin.java
+++ PigLatin.java
@@ -1,3 +1,21 @@
 public class PigLatin{
-  
+  public static String pigLatinSimple(String s){
+    if(s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u'){
+      return s+"hay";
+    }
+    else{
+      return s.substring(1)+s.charAt(0)+"ay";
+    }
+  }
+
+  // public static String pigLatin(String s){
+  //
+  // }
+  //
+  // public static String pigLatinBest(String s){
+  //
+  // }
+  public static void main(String[] args) {
+    System.out.println(pigLatinSimple(args[0]));
+  }
 }
